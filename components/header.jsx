3d106ed4bba38b8d7ed6fd6f9936e3332139c720
saloni9142 <kumarisaloni762@gmail.com>
2025-12-25 22:55:34 +0530

    new update

--- components/header.jsx
+++ components/header.jsx
@@ -1,13 +1,21 @@
 "use client";
-import React from 'react'
+import React, { useState } from 'react'
 import Image from "next/image";
 import Link from "next/link";
 import {  SignInButton, UserButton } from '@clerk/nextjs';
 import { Button } from './ui/button';
 import { Authenticated, Unauthenticated } from 'convex/react';
+import { BarLoader } from 'react-spinners';
+import { useStoreUser } from '@/hooks/use-store-user';
+import { Building, Plus, Ticket } from 'lucide-react';
 
 
-const header = () => {
+const Header = () => {
+
+ const { isLoading } = useStoreUser();
+ const [showUpgradeModal, setShowUpgradeModal] = useState(false);
+
+
   return (
     <>
     <nav className="fixed top-0 left-0 right-0 bg-background/80 backdrop-blur-xl z-20 border-b">
@@ -31,13 +39,55 @@ const header = () => {
               </Badge>
             )} */}
           </Link>
+          {/* search and location */}
 
  {/* Right Side Actions */}
      <div className='flex items-center'>
+      <Button
+                variant="ghost"
+                size="sm"
+                onClick={() => setShowUpgradeModal(true)}
+              >
+                Pricing
+              </Button>
+
+              <Button variant="ghost" size="sm" asChild className={"mr-2"}>
+              <Link href="/explore">Explore</Link>
+            </Button>
 
       <Authenticated>
         {/* create event */}
-              <UserButton />
+                  <Button size="sm" asChild className="flex gap-2 mr-4">
+                <Link href="/create-event">
+                  <Plus className="w-4 h-4" />
+                  <span className="hidden sm:inline">Create Event</span>
+                </Link>
+              </Button>
+
+
+             {/* User Button */}
+              <UserButton
+                afterSignOutUrl="/"
+                appearance={{
+                  elements: {
+                    avatarBox: "w-9 h-9",
+                  },
+                }}
+              >
+                <UserButton.MenuItems>
+                  <UserButton.Link
+                    label="My Tickets"
+                    labelIcon={<Ticket size={16} />}
+                    href="/my-tickets"
+                  />
+                  <UserButton.Link
+                    label="My Events"
+                    labelIcon={<Building size={16} />}
+                    href="/my-events"
+                  />
+                  <UserButton.Action label="manageAccount" />
+                </UserButton.MenuItems>
+              </UserButton>
             </Authenticated>
 <Unauthenticated>
               <SignInButton mode="modal">
@@ -48,11 +98,19 @@ const header = () => {
             
      </div>
 
+      </div>
+{/* {loader} */}
+           {isLoading && (
+          <div className="absolute bottom-0 left-0 w-full">
+            <BarLoader width={"100%"} color="#a855f7" />
           </div>
+        )}
           </nav>
+
+          {/* modals */}
 
     </>
   )
 }
 
-export default header
+export default Header
